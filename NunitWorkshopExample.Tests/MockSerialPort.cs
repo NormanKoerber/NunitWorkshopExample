3ddc13c659182ca8a2ad800c299a40a0a7e438f2
Norman Körber <norman.koerber@loehnert-elektronik.de>
2021-02-22 15:33:08 +0100

    Add PowerSupplyTests

---
+++ NunitWorkshopExample.Tests/MockSerialPort.cs
@@ -0,0 +1,31 @@
+﻿using NUnitWorkshopExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitWorkshopExample.Tests
+{
+    internal class MockSerialPort : ISerialPort
+    {
+        public bool IsOpen { get; private set; }
+        public string LastWrittenLine { get; private set; }
+        public string ReadLineData { get; set; } = "0";
+
+        public void Open()
+        {
+            IsOpen = true;
+        }
+
+        public string ReadLine()
+        {
+            return ReadLineData;
+        }
+
+        public void WriteLine(string sendData)
+        {
+            LastWrittenLine = sendData;
+        }
+    }
+}
---
+++ NunitWorkshopExample.Tests/PowerSupplyTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using NUnitWorkshopExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitWorkshopExample.Tests
+{
+    [TestFixture]
+    public class PowerSupplyTests
+    {
+        [Test]
+        public void ConstructorShouldThrowArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new PowerSupply(null));
+            Assert.That(ex.ParamName, Is.EqualTo("port"));
+        }
+
+        [Test]
+        public void ConstructorShouldOpenPort()
+        {
+            var serialPort = new MockSerialPort();
+
+            var powerSupply = new PowerSupply(serialPort);
+
+            Assert.That(serialPort.IsOpen);
+        }
+
+        [Test]
+        public void WritesVoltageValueCorrectToSerialPort()
+        {
+            var serialPort = new MockSerialPort();
+            var powerSupply = new PowerSupply(serialPort);
+
+            powerSupply.SetVoltage(13.7);
+
+            Assert.That(serialPort.LastWrittenLine, Is.EqualTo("SV137"));
+        }
+
+        [Test]
+        public void WhenPowerSupplyReturnAnErrorAnErrorNumberExcaptionIsThrown()
+        {
+            const string ErrorReturnValue = "1";
+            var serialPort = new MockSerialPort() { ReadLineData = ErrorReturnValue };
+            var powerSupply = new PowerSupply(serialPort);
+
+            Assert.Throws<ErrorNumberException>(() => powerSupply.SetVoltage(13.4));
+        }
+    }
+}
